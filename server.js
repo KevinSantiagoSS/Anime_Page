e3b3128164daf02864f27b6d70afa8fe3bccb3f4
KevinSantiagoSS <kserrano7@udi.edu.co>
2025-03-13 22:56:43 -0500

    Actualización

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/', (req, res) => {
 
 // Iniciar servidor
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`✅ Servidor corriendo en el puerto ${PORT}`);
 });
